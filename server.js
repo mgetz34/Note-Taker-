309215c57a98dc2c8d70565792b87fa59afb1339
mgetz34 <mgetz340@gmail.com>
2022-11-28 19:28:01 -0700

    worked on api get and post paths

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const fs = require('fs');
 // const uuid = require('./helpers/uuid')
-
+const data = require('./db/db.json');
+const path = require('path');
 const PORT = 3001;
-
 const app = express();
 
 app.use(express.json());
@@ -11,21 +11,28 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-//sets index.html as home page 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+//sets index.html http://localhost:3001 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+//sets notes.html http://localhost:3001/notes
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+    res.json(data);
 })
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
+app.post("/api/notes", (req, res) => {
+    noteData = req.body
 })
 
-app.get('', (req, res) => {
-
-})
-
-app.post('', (req, res) => {
-
+fs.appendFile(".db/db.json", JSON.stringify(noteData), function (err) {
+    const err = "File cannot be read"
+    if (err) throw err;
+    console.log("Saved!");
 })
 
 app.listen(PORT, () =>
