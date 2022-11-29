1f9136583ce53f0e6714c35307a927821b157ac7
mgetz34 <mgetz340@gmail.com>
2022-11-28 19:53:57 -0700

    troubleshooting pathways :qa

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-//sets index.html http://localhost:3001 
+//sets index.html to http://localhost:3001 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-//sets notes.html http://localhost:3001/notes
+//sets notes.html to http://localhost:3001/notes
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
@@ -26,14 +26,13 @@ app.get("/api/notes", (req, res) => {
 })
 
 app.post("/api/notes", (req, res) => {
-    noteData = req.body
+    noteData = req.body;
 })
 
-fs.appendFile(".db/db.json", JSON.stringify(noteData), function (err) {
-    const err = "File cannot be read"
-    if (err) throw err;
-    console.log("Saved!");
-})
+// fs.appendFile(".db/db.json", JSON.stringify(noteData), function (err) {
+//     if (err) throw err;
+//     console.log("Saved!");
+// })
 
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT} 🚀`)
