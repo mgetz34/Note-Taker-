761a1aea9803decfac9c43809fafe439dc2794c9
mgetz34 <mgetz340@gmail.com>
2022-11-28 18:40:01 -0700

    added starter to server js boilerplate

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const uuid = require('./helpers/uuid')
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
