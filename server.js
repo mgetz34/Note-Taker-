a17e72fafdff3cf38ef682f11d9cdcf5c1c0ab72
mgetz34 <mgetz340@gmail.com>
2022-11-29 16:23:34 -0700

    changed server.js line spacing

--- server.js
+++ server.js
@@ -22,13 +22,11 @@ app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
-
 //http://localhost:3001/api/notes
 //accesses ds.db.json
 app.get("/api/notes", (req, res) => {
     res.json(data);
 })
-
 
 app.post("/api/notes", (req, res) => {
     console.info(`${req.method} request received to add a note`);
@@ -46,6 +44,7 @@ app.post("/api/notes", (req, res) => {
         };
 
         res.json(newNote);
+
         const notesArr = [...data, newNote]
         // Convert the data to a string so we can save it
         const noteString = JSON.stringify(newNote);
@@ -61,6 +60,7 @@ app.post("/api/notes", (req, res) => {
         res.status(500).json('Error in posting note');
     };
 });
+
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
