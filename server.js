64e023e3a2a50a3d0abbaacd9ef01c318cbdff53
mgetz34 <mgetz340@gmail.com>
2022-11-28 18:57:50 -0700

    added a post listner to server and worked on notes.html path

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-
-const uuid = require('./helpers/uuid')
+const fs = require('fs');
+// const uuid = require('./helpers/uuid')
 
 const PORT = 3001;
 
@@ -11,3 +11,23 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
+//sets index.html as home page 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+})
+
+app.get('', (req, res) => {
+
+})
+
+app.post('', (req, res) => {
+
+})
+
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
